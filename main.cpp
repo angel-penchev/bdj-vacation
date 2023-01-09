eb9f61cf8dc47491d070468b128f52b80047bbfe
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2023-01-09 16:01:20 +0200

    [Feat] Cities user input
    Added handling for user input.

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    size_t citiesCount;
+    std::cout << "Брой на градовете през които преминава пътническия влак: ";
+    std::cin >> citiesCount;
+
+    std::vector<std::string> cities;
+    std::cout << "Имена на градовете: ";
+    for (size_t i = 0; i < citiesCount; ++i) {
+        std::string city;
+        std::cin >> city;
+        cities.push_back(city);
+    }
+
+    size_t directConnectionsCount;
+    std::cout << "Брой на директните връзки между градовете: ";
+    std::cin >> directConnectionsCount;
+
+    std::vector<std::pair<std::string, std::string>> directConnections;
+    std::cout << "Директни връзки: ";
+    for (size_t i = 0; i < directConnectionsCount; ++i) {
+        std::string city1, city2;
+        std::cin >> city1 >> city2;
+        directConnections.push_back(std::make_pair(city1, city2));
+    }
+
+    return 0;
+}
